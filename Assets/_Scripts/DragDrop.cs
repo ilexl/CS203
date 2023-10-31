cfce798dc57d40b0563b35b213c7414941ecfee9
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-10-31 13:54:22 +1300

    Move SetAsLastSibling to OnBeginDrag for performance

--- Assets/_Scripts/DragDrop.cs
+++ Assets/_Scripts/DragDrop.cs
@@ -29,11 +29,11 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, I
     public void OnDrag(PointerEventData eventData)
     {
 
-        transform.SetAsLastSibling(); //ensure it renders above all other tiles
         actualPosition = GameToCanvas(eventData.position);
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
+        transform.SetAsLastSibling(); //ensure it renders above all other tiles
         lastDropHolder = dropHolder;
         dropHolder = null;
         canvasGroup.blocksRaycasts = false;
