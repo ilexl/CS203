ded165cef53ad39b888bbec0bf645aa5a58a7c11
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-10 13:01:27 +1300

    Multi player changes

---
+++ Assets/_Scripts/SERVER/Classes/Lobby.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using wfMultiplayer;
+
+namespace wfMultiplayer
+{
+    public class Lobby
+    {
+        private List<List<char>> gameBoard;
+        private List<Player> players;
+        int playerTurnIndex;
+
+        public Lobby(List<List<char>> gameBoard, List<Player> players)
+        {
+            this.gameBoard = gameBoard;
+            this.players = players;
+            playerTurnIndex = 0;
+        }
+        public List<List<char>> GetGameBoard() { return gameBoard; }
+
+        public char GetGameBoardItem(int x, int y) { return gameBoard[x][y]; }
+
+        public void SetGameBoardItem(int x, int y, char c)
+        {
+            gameBoard[x][y] = c;
+        }
+    }
+}
---
+++ Assets/_Scripts/SERVER/Classes/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace wfMultiplayer
+{
+    public class Player
+    {
+        public string ip { get; set; }
+        public string username { get; set; }
+
+        public Player(string ip, string username)
+        {
+            this.ip = ip;
+            this.username = username;
+        }
+    }
+}
+
