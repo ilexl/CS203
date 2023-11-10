6478006c670f38d37c76eb992e4e07715fe67995
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-10 14:06:39 +1300

    MatchList, creation functionality

--- Assets/_Scripts/SERVER/MatchList.cs
+++ Assets/_Scripts/SERVER/MatchList.cs
@@ -7,5 +7,27 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MatchList : MonoBehaviour
 {
+    public List<Match> matchList;
 
 }
+    /*****************************Lobby Creation*****************************/
+
+    public void CreateNewMatch(List<List<char>> gameBoard, List<wfMultiplayer.Player> players)
+    {
+        Match match = new Match(gameBoard, players);
+        Debug.Log($"Creating match {matchList.Count}, {match}");
+        matchList.Add(match);
+    }
+
+    private void LogNewLobby(List<wfMultiplayer.Player> players)
+    {
+        string playerText = "";
+        foreach (var player in players)
+        {
+            playerText += player.ToString() + ", ";
+        }
+        playerText = playerText.Substring(0, playerText.Length - 3);
+        Debug.Log($"Creating match {matchList.Count} with players: " + playerText);
+
+    }
+
