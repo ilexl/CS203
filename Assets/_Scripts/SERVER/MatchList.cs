36ff596bb33755b03eb78a10a6969ecd252feb54
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-11-13 13:27:27 +1300

    Remove a random namespace that visual studio decided to add

--- Assets/_Scripts/SERVER/MatchList.cs
+++ Assets/_Scripts/SERVER/MatchList.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using wfMultiplayer;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class MatchList : MonoBehaviour
 {
