f3b9b3750511a3a328181b0735ad51b9f044000f
Alex <109491531+ilexl@users.noreply.github.com>
2023-11-16 00:57:42 +1300

    Function Outlines For Chat

--- Assets/_Scripts/ChatManager.cs
+++ Assets/_Scripts/ChatManager.cs
@@ -5,10 +5,46 @@ using UnityEngine;
 public class ChatManager : MonoBehaviour
 {
     [SerializeField] GameObject offlineOverlay;
+    private static ChatManager _singleton;
+    public static ChatManager Singleton
+    {
+        get => _singleton;
+        private set
+        {
+            if (_singleton == null)
+                _singleton = value;
+            else if (_singleton != value)
+            {
+                Debug.Log($"{nameof(ChatManager)} instance already exists, destroying duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+
+    public enum SentBy
+    {
+        Player, 
+        Opponent,
+    }
+
+    private void Awake()
+    {
+        Singleton = this;
+    }
 
     // Update is called once per frame
     void Update()
     {
         offlineOverlay.SetActive(!Game.Singleton.isMultiplayer);
+    }
+
+    public void ClearAllMessages()
+    {
+
+    }
+
+    public void Message(string message, SentBy sentBy)
+    {
+
     }
 }
