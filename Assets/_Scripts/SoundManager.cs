66fd4445930aabadfc5775f39fedcd097b635b92
Alex <109491531+ilexl@users.noreply.github.com>
2023-11-16 14:26:28 +1300

    button sounds

--- Assets/_Scripts/SoundManager.cs
+++ Assets/_Scripts/SoundManager.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
@@ -8,5 +12,14 @@ public class SoundManager : MonoBehaviour
     public void PlaySound(int soundIndex)
     {
         Instantiate(soundPrefabs[soundIndex], transform);
+    }
+
+    public void Start()
+    {
+        var names = FindObjectsOfType<Button>(includeInactive: true).ToList();
+        foreach(Button b in names)
+        {
+            b.onClick.AddListener(() => PlaySound(0));
+        }
     }
 }
